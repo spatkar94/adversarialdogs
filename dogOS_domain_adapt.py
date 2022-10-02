1f6db8e9bd1613964267e4a3fe47999f4607722f
spatkar94 <37498091+spatkar94@users.noreply.github.com>
2022-10-02 17:32:17 -0400

    Update dogOS_domain_adapt.py

--- dogOS_domain_adapt.py
+++ dogOS_domain_adapt.py
@@ -1,17 +1,52 @@
 # Contributor: Sushant Patkar
 # Email: patkar.sushant@nih.gov
-# Artificial Intelligence Resource, National Cancer Institute
 # Sept 30, 2022
-# THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS "AS IS" FOR THE PURPOSES OF ACADEMIC RESEARCH 
-# ONLY AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-# WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
-# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
-# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# By downloading or otherwise receiving the SOFTWARE, RECIPIENT may 
+# use and/or redistribute the SOFTWARE, with or without modification, 
+# subject to RECIPIENT’s agreement to the following terms:
+# 
+# 1. THE SOFTWARE SHALL NOT BE USED IN THE TREATMENT OR DIAGNOSIS 
+# OF CANINE OR HUMAN SUBJECTS.  RECIPIENT is responsible for 
+# compliance with all laws and regulations applicable to the use 
+# of the SOFTWARE.
+# 
+# 2. The SOFTWARE that is distributed pursuant to this Agreement 
+# has been created by United States Government employees. In 
+# accordance with Title 17 of the United States Code, section 105, 
+# the SOFTWARE is not subject to copyright protection in the 
+# United States.  Other than copyright, all rights, title and 
+# interest in the SOFTWARE shall remain with the PROVIDER.   
+# 
+# 3.	RECIPIENT agrees to acknowledge PROVIDER’s contribution and 
+# the name of the author of the SOFTWARE in all written publications 
+# containing any data or information regarding or resulting from use 
+# of the SOFTWARE. 
+# 
+# 4.	THE SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED 
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE PROVIDER OR THE INDIVIDUAL DEVELOPERS 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE.  
+# 
+# 5.	RECIPIENT agrees not to use any trademarks, service marks, trade names, 
+# logos or product names of NCI or NIH to endorse or promote products derived 
+# from the SOFTWARE without specific, prior and written permission.
+# 
+# 6.	For sake of clarity, and not by way of limitation, RECIPIENT may add its 
+# own copyright statement to its modifications or derivative works of the SOFTWARE 
+# and may provide additional or different license terms and conditions in its 
+# sublicenses of modifications or derivative works of the SOFTWARE provided that 
+# RECIPIENT’s use, reproduction, and distribution of the SOFTWARE otherwise complies 
+# with the conditions stated in this Agreement. Whenever Recipient distributes or 
+# redistributes the SOFTWARE, a copy of this Agreement must be included with 
+# each copy of the SOFTWARE.
 
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
